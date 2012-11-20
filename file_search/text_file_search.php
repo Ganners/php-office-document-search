928a30194bd7443ef5358e8e3c88a26c59ede8c8
Mark Gannaway <mark@cyb.co.uk>
2012-11-20 13:49:54 +0000

    Added a text based file search

---
+++ file_search/text_file_search.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace File_Search;
+
+use \Exception;
+use \File_Search\File_SearchInterface;
+
+class Text_File_Search implements File_SearchInterface {
+
+    private $_fileName = '';
+    private $_stringFound = FALSE;
+
+    /**
+     * The constructor for the office search
+     * @param Filename - This takes a single filename of an office document format
+     */
+    public function __construct($filename) {
+        if(file_exists($filename)) {
+            $this->_fileName = $filename;
+        } else {
+            throw new Exception("File {$filename} could not be found. Please ensure the path is correct and this file exists.");
+        }
+    }
+
+    /**
+     * Sets a search string to look for within a file and performs the search
+     * @param  string $string
+     * @return bool if matched
+     */
+    public function searchFileForString($string) {
+        $this->_searchString($string);
+        return $this->_stringFound;
+    }
+
+    /**
+     * Performs the magic, actually does the search
+     * @param  string $string The search string
+     */
+    private function _searchString($string) {
+        $file = fopen($this->_fileName, 'r');
+        $file_contents = fread($file, filesize($this->_fileName));
+        if(stristr($file_contents, $string)) {
+            $this->_stringFound = TRUE;
+        }
+        fclose($file);
+        return TRUE;
+    }
+
+}
